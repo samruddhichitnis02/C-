e857abeddc06a30f43d778d28f134021ca3493e9
Samruddhi Chitnis <42942700+samruddhichitnis02@users.noreply.github.com>
2025-01-15 09:16:47 +0530

    Basics of Comments and Variables

---
+++ Variables_Comments.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+// This program was created by Samruddhi 
+/*
+This
+is 
+a 
+multiline
+comment
+*/
+int main(){
+    int sum = 6; 
+    cout << "Hello C++" << sum;
+    return 0;
+}
