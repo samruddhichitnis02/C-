318a267bf88b8d35967011e3e498608337ff4fbe
Samruddhi Chitnis <42942700+samruddhichitnis02@users.noreply.github.com>
2025-01-25 00:21:05 +0530

    String Variable

---
+++ Strings.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string text = "Hello this is a string";
+    string text1 = "Hello";
+    string text2 = "Friend";
+    string concat = text1 + " " + text2 + " " + "This is a C++ Program";
+
+
+    cout << text << endl;
+    cout << text1 << endl;
+    cout << text2 << endl;
+    cout << concat << endl;
+    return 0;
+}
